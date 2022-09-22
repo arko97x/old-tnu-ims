09620f7933d4a9c427fbe28040c2a2bde86ef5c0
Arko <bhattacharjee.arkoprabho@gmail.com>
2022-09-23 00:09:05 +0530

    Fixed README.md

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c56da35611331e0"; }
+/******/ 		__webpack_require__.h = function() { return "cb0d88fd1d832ee7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
