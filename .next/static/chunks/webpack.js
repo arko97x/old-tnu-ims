94ee1f83b6a793ea8a3e064de5f524db0e2c4bdb
Arko <bhattacharjee.arkoprabho@gmail.com>
2022-09-22 23:57:13 +0530

    Updated the favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "38a5b0827af34933"; }
+/******/ 		__webpack_require__.h = function() { return "7c56da35611331e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
